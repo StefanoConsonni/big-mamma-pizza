1f5280d3012000cdf8b8982775486174b200612e
Stefano Consonni <stefanoconsonni@ymail.com>
2023-10-16 16:43:21 +0200

    add Menu.tsx + MenuItem.tsx

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,16 @@ export type TItem = {
 export type TCartItemProps = {
   item: TItem;
 };
+
+export type TMenuItem = {
+  pizza: TPizza;
+};
+
+export type TPizza = {
+  id: string;
+  name: string;
+  unitPrice: number;
+  ingredients: string[];
+  soldOut: boolean;
+  imageUrl: string;
+};
